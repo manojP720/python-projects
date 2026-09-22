a52fd4f9faa00cbd32cc6837cc8b7f3250fef347
manoj7 <271135612+manojP720@users.noreply.github.com>
2026-09-22 16:54:27 +0530

    dsa

--- struct.c
+++ struct.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
-struct structurename {
-    dataType member1;
-    dataType member2;
+struct Employee {
+    
+    int id;
+    char name[100];
+    float salary;
 };
 
 void main ()
 {
+    struct Employee e1;
+    struct Employee e2;
+
+
+    e1.id = 1;
+    e1.salary = 50000.0;
+
+    printf(" ");
 
 }
