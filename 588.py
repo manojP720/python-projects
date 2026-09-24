5d10594295f07647c926c361794c58fb2ab271c4
manoj7 <271135612+manojP720@users.noreply.github.com>
2026-09-24 21:21:27 +0530

    DSA

--- 588.py
+++ 588.py
@@ -1,1 +1,11 @@
-["flower", "flow", "flight"]
+def longestCommonPrefix(strs):
+    prefix = strs[0]
+
+    for i in range(1, len(strs)):
+        while not strs[i].startswith(prefix):
+            prefix = prefix[:-1]
+
+            if prefix == "":
+                return ""
+
+    return prefix
